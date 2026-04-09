d84bc76f612ea1ef52de26297c7872318a569caf
suhaibmowaffaqaziz-tech <suhaibmowaffaqaziz@gmail.com>
2026-04-09 21:40:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+function checkAnswer(btn, correct) {
+  const parent = btn.parentElement;
+  const buttons = parent.querySelectorAll('button');
+  buttons.forEach(b => b.style.pointerEvents = 'none');
+
+  if (correct) {
+    btn.classList.add('correct');
+    btn.innerHTML += " ✨ (إجابة صحيحة)";
+  } else {
+    btn.classList.add('wrong');
+    btn.innerHTML += " ❌ (إجابة خاطئة)";
+  }
+}
